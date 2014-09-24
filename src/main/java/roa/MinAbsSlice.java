258f50dc0b45085b2e807af678d15add3ea560f0
Radu Ometita - Adrian <radu.ometita@fortech.ro>
2014-09-24 19:57:32 +0300

    Noticed that the partitions can have only one item.

--- src/main/java/roa/MinAbsSlice.java
+++ src/main/java/roa/MinAbsSlice.java
@@ -24,8 +24,14 @@ public class MinAbsSlice {
 
         if (a.length >= 3) {
             min = Math.max(sum(a, 0, 1), min);
+            min = Math.max(sum(a, 1, 2), min);
             min = Math.min(sum(a, 0, 2), min);
+            min = Math.min(sum(a, 2, 3), min);
             min = Math.min(sum(a, 1, 3), min);
+        }
+
+        if (a.length >= 4) {
+
         }
 
         return min;
