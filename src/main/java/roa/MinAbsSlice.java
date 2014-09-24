9aced28dda1c8349cf94662734c4dbb53dce9c27
Radu Ometita - Adrian <radu.ometita@fortech.ro>
2014-09-24 20:01:17 +0300

    More tests failing for no reason at all. Maybe one good reason would be using Math.max() instead of Math.min().

--- src/main/java/roa/MinAbsSlice.java
+++ src/main/java/roa/MinAbsSlice.java
@@ -23,13 +23,13 @@ public class MinAbsSlice {
         int min = sum(a, 0, a.length);
 
         if (a.length >= 2) {
-            min = Math.max(sum(a, 0, 1), min);
-            min = Math.max(sum(a, 1, 2), min);
+            min = Math.min(sum(a, 0, 1), min);
+            min = Math.min(sum(a, 1, 2), min);
         }
 
         if (a.length >= 3) {
-            min = Math.max(sum(a, 0, 1), min);
-            min = Math.max(sum(a, 1, 2), min);
+            min = Math.min(sum(a, 0, 1), min);
+            min = Math.min(sum(a, 1, 2), min);
             min = Math.min(sum(a, 0, 2), min);
             min = Math.min(sum(a, 2, 3), min);
             min = Math.min(sum(a, 1, 3), min);
@@ -68,6 +68,6 @@ public class MinAbsSlice {
 
     @Test
     public void testTwoPositiveElements() {
-        assertThat(solution(1, 2), is(3));
+        assertThat(solution(1, 2), is(1));
     }
 }
